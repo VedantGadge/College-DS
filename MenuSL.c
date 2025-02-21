3d5790ab841cc3cc9a27866b4ff4d2d8df0a95a7
Vedant Gadge <vedant.gadgegsis@gmail.com>
2025-02-21 09:43:25 +0530

    Add files via upload

--- MenuSL.c
+++ MenuSL.c
@@ -83,6 +83,104 @@ void insertNodeBF( int data,int target){
 
 }  
 
+void deleteLastNode() {
+    if (head == NULL) {
+        printf("List is empty. No node to delete.\n");
+        return;
+    }
+    
+    if (head->next == NULL) { // If only one node is present
+        free(head);
+        head = NULL;
+        printf("Last node deleted, the list is now empty.\n");
+        return;
+    }
+
+    struct Node* temp = head;
+    struct Node* prev = NULL;
+
+    while (temp->next != NULL) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    prev->next = NULL;  
+    free(temp);         
+    printf("Last node deleted successfully.\n");
+}
+
+void deleteFirstNode() {
+    if (head == NULL) {
+        printf("List is empty. No node to delete.\n");
+        return;
+    }
+    
+    if (head->next == NULL) { // If only one node is present
+        free(head);
+        head = NULL;
+        printf("Last node deleted, the list is now empty.\n");
+        return;
+    }
+
+    struct Node* temp = head;
+    head = head->next;   
+    free(temp);  
+    printf("First node deleted successfully.\n");
+}
+
+void deleteNode(int target) {
+    if (head == NULL) {
+        printf("List is empty. No node to delete.\n");
+        return;
+    }
+    
+    if (head->next == NULL) { // If only one node is present
+        free(head);
+        head = NULL;
+        printf("Last node deleted, the list is now empty.\n");
+        return;
+    }
+
+    struct Node* temp = head;
+    struct Node* prev = NULL;
+
+    while (temp->next != target) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    prev->next = temp->next;
+    free(temp);
+    printf("Node of data %d has been deleted! \n",target);
+
+}
+
+void deleteNodeBF(int target) {
+    if (head == NULL) {
+        printf("List is empty. No node to delete.\n");
+        return;
+    }
+    
+    if (head->next == NULL) { // If only one node is present
+        free(head);
+        head = NULL;
+        printf("Last node deleted, the list is now empty.\n");
+        return;
+    }
+
+    struct Node* temp = head;
+    struct Node* prev = NULL;
+
+    while (temp->next->data != target) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    prev->next = temp->next;
+    free(prev);
+    printf("Node of data %d has been deleted! \n",target);
+}
+
 void main(){
     
     int n,target,choice;
@@ -95,7 +193,12 @@ void main(){
         printf("Enter 2 to append in the Linked List\n");
         printf("Enter 3 to insert before a node in the Linked List\n");
         printf("Enter 4 to insert after a node in the Linked List\n");
-        printf("Enter 5 to terminate operations\n");
+        printf("Enter 5 to delete the last node in the Linked List\n");
+        printf("Enter 6 to delete the first node in the Linked List\n");
+        printf("Enter 7 to delete a node in the Linked List\n");
+        printf("Enter 8 to delete after a node  in the Linked List\n");
+        printf("Enter 9 to delete before a node in the Linked List\n");
+        printf("Enter 10 to terminate operations\n");
         scanf("%d", &choice);
     
         switch (choice)
@@ -137,10 +240,47 @@ void main(){
             break;
 
         case 5:
+             // Delete the last node
+             deleteLastNode();
+             printList();
+             break;
+
+        case 6:
+             // Delete the first node
+             deleteFirstNode();
+             printList();
+             break;
+
+        case 7:
+             // Delete specific node
+             printf("Enter the target node data to delete : \n");
+             scanf("%d", &target);
+             deleteNode(target);
+             printList();
+             break;
+
+        case 8:
+             // Delete after node
+             printf("Enter the target node data to delete : \n");
+             scanf("%d", &target);
+            //  deleteNode(target);
+             printList();
+             break;
+
+        case 9:
+             // Delete before node
+            printf("Enter the target node data to delete before: \n");
+            scanf("%d", &target);
+            deleteNodeBF(target);
+            printList();
+            break;
+
+        case 10:
             // Close Menu
             printList();
             printf("Terminating...\n");
             cont=0;
+            break;
 
         default:
             printf("Enter a valid option!! \n");
